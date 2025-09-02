a47dfeedc8ba58835f1689a2fb8836d77cb10dbc
gusstass <gustavosasi@uni9.edu.br>
2025-09-02 00:51:53 +0000

    feat: add projeto

--- Televisao.cs
+++ Televisao.cs
@@ -1,5 +1,6 @@
 public class Televisao
 {
+    private const int VOL_MAX=100;
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
@@ -7,12 +8,15 @@ public class Televisao
 
     public float Tamanho{get;} //removendo o set; para que seja lido porem não escrito por um código externo.
     public int Resolucao{get; set;}
-    public int Volume{get; set;} //
+    public int Volume{get; private set;} //privando o set para nao ter alterações externas
     public int Canal{get; set;}
     public bool Estado{get; set;}
 
     public void AumentarVolume()
     {
+        if(Volume < VOL_MAX)
         Volume = Volume+1; //Volume++ 
+        else
+        Console.WriteLine("TV está no máximo");
     }
 }
