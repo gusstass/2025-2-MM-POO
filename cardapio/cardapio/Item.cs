d201438f3414e5295aac6f45e9f1da08c68b235d
gusstass <gustavosasi@uni9.edu.br>
2025-09-23 00:32:48 +0000

    feat:sadh

---
+++ cardapio/cardapio/Item.cs
@@ -0,0 +1,7 @@
+using System.Dynamic;
+public class Item
+{
+    public string Nome { get; set; }
+    public List<string> Ingredientes { get; set; }
+    public decimal Valor { get; set; }
+}
---
+++ cardapio/cardapio/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var todosDias = new Disponibilidade
+{
+    Dias = new List<int>
+    {
+        (int)DayOfWeek.Sunday,
+        (int)DayOfWeek.Monday,
+        (int)DayOfWeek.Tuesday,
+        (int)DayOfWeek.Wednesday,
+        (int)DayOfWeek.Thursday,
+        (int)DayOfWeek.Friday,
+        (int)DayOfWeek.Saturday,
+        (int)DayOfWeek.Sunday
+    },
+    HoraInicio = new TimeSpan(0, 0, 0),
+    HoraFim = new TimeSpan(23, 59, 59),
+};
+Console.WriteLine("Todos os dias: {0}", todosDias.EstaDisponivel());
